49efbe5d665fda94c0547c9f1ab09bd056eaf7c9
wz2 <1026623522@qq.com>
2023-11-07 00:36:03 +0300

    for test 2.0

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 
 int main(int argc, char **argv)
 {
-     char first[255], last[255];
-     askname(first, last);
+     char first[255], last[255];/* for the test2.3.9 */
+     askname(first, last);/* for the test2.3.9 */
      printf("Hello, %s %s!\n", first, last);/* for the test2.3.9 */
      return 0;
 }
